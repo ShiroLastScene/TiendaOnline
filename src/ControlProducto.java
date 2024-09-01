200c440f31ca21f1461a41e51860ef54d071e1b6
ShiroLastScene <eduardoespinoza@live.cl>
2024-08-31 23:41:07 -0400

    Se agregan metodos

--- src/ControlProducto.java
+++ src/ControlProducto.java
@@ -21,6 +21,78 @@ public class ControlProducto {
         productos.add(nuevoProducto);
     }
 
+    // Método para cambiar el precio de un producto
+    public void cambiarPrecio(int id, double valor) {
+        for (Producto producto : productos) {
+            if (producto.getId() == id) {
+                producto.setPrecio(valor);
+                System.out.println("Precio del producto actualizado.");
+                return;
+            }
+        }
+        System.out.println("Producto no encontrado, no es posible cambiar el precio.");
+    }
+
+    // Método para cambiar la categoria de un producto
+    public void cambiarCategoria(int id, String categoria) {
+        for (Producto producto : productos) {
+            if (producto.getId() == id) {
+                producto.setCategoria(categoria);
+                System.out.println("Categoria del producto actualizada.");
+                return;
+            }
+        }
+        System.out.println("Producto no encontrado, no es posible cambiar la categoria.");
+    }
+
+    // Método para cambiar el formato de un ProductoDigital
+    public void cambiarFormato(int id, String formato) {
+        try
+        {
+            for (Producto producto : productos) {
+                if (producto.getId() == id) {
+                    if (producto instanceof ProductoDigital) {
+                        ProductoDigital productoDigital = (ProductoDigital) producto;
+                        productoDigital.setFormato(formato);
+                        System.out.println("Formato del producto digital actualizado.");
+                        return;
+                    } else {
+                        System.out.println("El producto no es un producto digital.");
+                        return;
+                    }
+                }
+            }
+            System.out.println("Producto no encontrado, no es posible cambiar el formato.");
+        } catch (Exception e) {
+            //Lanza la excepción
+            ControlErrores.InformarError(e);
+        }
+    }
+
+    // Método para cambiar el tamaño de un ProductoDigital
+    public void cambiarTamano(int id, String tamano) {
+        try
+        {
+            for (Producto producto : productos) {
+                if (producto.getId() == id) {
+                    if (producto instanceof ProductoDigital) {
+                        ProductoDigital productoDigital = (ProductoDigital) producto;
+                        productoDigital.setTamanoArchivo(tamano);
+                        System.out.println("Tamaño del producto digital actualizado.");
+                        return;
+                    } else {
+                        System.out.println("El producto no es un producto digital.");
+                        return;
+                    }
+                }
+            }
+            System.out.println("Producto no encontrado, no es posible cambiar el tamaño.");
+        } catch (Exception e) {
+            //Lanza la excepción
+            ControlErrores.InformarError(e);
+        }
+    }
+
     // Buscar producto por nombre
     public void buscarProductoPorNombre(String nombre) {
         try
@@ -28,20 +100,28 @@ public class ControlProducto {
             boolean encontrado;
             encontrado=false;
 
-            System.out.println("Buscando producto " + nombre + "...");
+            System.out.println("Buscando el producto " + nombre + "...");
 
             for (Producto producto : productos) {
                 if (producto.getNombre().equalsIgnoreCase(nombre)) {
                     encontrado=true;
+                    System.out.println("=============================================");
                     System.out.println("Producto encontrado: ");
                     System.out.println("id: " + producto.getId());
                     System.out.println("nombre: " + nombre);
                     System.out.println("precio: " + producto.getPrecio());
                     System.out.println("categoria: " + producto.getCategoria());
+
+                    // Verifica si el producto es un producto digital
+                    if (producto instanceof ProductoDigital productoDigital) {
+                        System.out.println("tamaño: " + productoDigital.getTamanoArchivo());
+                        System.out.println("formato: " + productoDigital.getFormato());
+                    }
+                    System.out.println("=============================================");
                 }
             }
             if (!encontrado) {
-                System.out.println("No existe el producto encontrado.");
+                System.out.println("No existe el producto.");
             }
         } catch (Exception e) {
             //Lanza la excepción
@@ -56,6 +136,6 @@ public class ControlProducto {
     }
     public double calcularConDescuento(double precio, double porcentaje){
 
-        return precio * (porcentaje/100);
+        return precio * ((100-porcentaje)/100);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,15 @@ public class Main {
         System.out.println("Usuario agrega el producto");
 
         controlProducto.agregarProducto("PlayStation 6",799999, "Consolas");
-        controlProducto.agregarProducto("BaldursGate 4",49999, "Juego virtual","140GB","ISO");
+        controlProducto.agregarProducto("BaldursGate 4",49999, "Juego virtual","140GB","ISI");
         controlProducto.buscarProductoPorNombre("PlayStation 6");
+        controlProducto.cambiarFormato(1,"ISO");
+        controlProducto.cambiarFormato(2,"ISO");
+        controlProducto.buscarProductoPorNombre("BaldursGate 4");
 
+        System.out.println("Cambiamos precio aplicando un 7% de descuento");
+        controlProducto.cambiarPrecio(1,controlProducto.calcularConDescuento(799999,7));
+        controlProducto.buscarProductoPorNombre("PlayStation 6");
 
     }
 }
