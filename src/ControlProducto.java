b3be58a0420fe4f8a265b02a0c26ec33321e5b6c
ShiroLastScene <eduardoespinoza@live.cl>
2024-09-02 19:32:25 -0400

    Se actualizan metodos de ControlProducto

--- src/ControlProducto.java
+++ src/ControlProducto.java
@@ -93,6 +93,43 @@ public class ControlProducto {
         }
     }
 
+    // Buscar producto por ID
+    public void buscarProductoPorID(int id) {
+        try
+        {
+            boolean encontrado;
+            encontrado=false;
+
+            System.out.println();
+            System.out.println("Buscando el producto ID " + id + "...");
+
+            for (Producto producto : productos) {
+                if (producto.getId() == id) {
+                    encontrado=true;
+                    System.out.println("=============================================");
+                    System.out.println("Producto encontrado: ");
+                    System.out.println("id: " + id);
+                    System.out.println("nombre: " + producto.getNombre());
+                    System.out.println("precio: " + producto.getPrecio());
+                    System.out.println("categoria: " + producto.getCategoria());
+
+                    // Verifica si el producto es un producto digital
+                    if (producto instanceof ProductoDigital productoDigital) {
+                        System.out.println("tamaño: " + productoDigital.getTamanoArchivo());
+                        System.out.println("formato: " + productoDigital.getFormato());
+                    }
+                    System.out.println("=============================================");
+                }
+            }
+            if (!encontrado) {
+                System.out.println("No existe el producto.");
+            }
+        } catch (Exception e) {
+            //Lanza la excepción
+            ControlErrores.InformarError(e);
+        }
+    }
+
     // Buscar producto por nombre
     public void buscarProductoPorNombre(String nombre) {
         try
@@ -100,10 +137,11 @@ public class ControlProducto {
             boolean encontrado;
             encontrado=false;
 
+            System.out.println();
             System.out.println("Buscando el producto " + nombre + "...");
 
             for (Producto producto : productos) {
-                if (producto.getNombre().equalsIgnoreCase(nombre)) {
+                if (producto.getNombre().contains(nombre)) {
                     encontrado=true;
                     System.out.println("=============================================");
                     System.out.println("Producto encontrado: ");
@@ -128,6 +166,33 @@ public class ControlProducto {
             ControlErrores.InformarError(e);
         }
     }
+
+
+    // Muestra una lista de los productos
+    public void listarProductos() {
+        try {
+            System.out.println();
+
+            if (productos.isEmpty()) {
+                System.out.println("No hay productos en la lista.");
+                return;
+            }
+
+            System.out.println("Lista de productos:");
+            System.out.println("=============================================");
+
+            for (Producto producto : productos) {
+                System.out.println(producto.getId() + " : " + producto.getNombre());
+            }
+
+            System.out.println("=============================================");
+        } catch (Exception e) {
+            // Manejar excepciones
+            ControlErrores.InformarError(e);
+        }
+    }
+
+
     public double calcularConDescuento(double precio){
         double porcentaje;
         porcentaje = 0.10; //10% de descuento
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,17 @@ public class Main {
         ControlProducto controlProducto = new ControlProducto();
         System.out.println("Usuario agrega el producto");
 
+        controlProducto.agregarProducto("PlayStation 5",499999, "Consolas");
         controlProducto.agregarProducto("PlayStation 6",799999, "Consolas");
         controlProducto.agregarProducto("BaldursGate 4",49999, "Juego virtual","140GB","ISI");
         controlProducto.buscarProductoPorNombre("PlayStation 6");
         controlProducto.cambiarFormato(1,"ISO");
         controlProducto.cambiarFormato(2,"ISO");
-        controlProducto.buscarProductoPorNombre("BaldursGate 4");
-
+        controlProducto.buscarProductoPorID(3);
         System.out.println("Cambiamos precio aplicando un 7% de descuento");
         controlProducto.cambiarPrecio(1,controlProducto.calcularConDescuento(799999,7));
-        controlProducto.buscarProductoPorNombre("PlayStation 6");
+        controlProducto.buscarProductoPorNombre("PlayStation");
+        controlProducto.listarProductos();
 
     }
 }
